7d815d50223f4637b8af8b4f7b66898a31c42301
Achraf10-bit <achrafbouasria02@gmail.com>
2025-03-31 19:35:20 +0100

    Simplify Vercel deployment configuration

--- api/index.php
+++ api/index.php
@@ -1,5 +1,30 @@
 <?php
 
+// Handle static files
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
+    $path = __DIR__ . '/../public' . $uri;
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+    
+    $mime_types = [
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'ico' => 'image/x-icon',
+    ];
+    
+    if (isset($mime_types[$ext])) {
+        header('Content-Type: ' . $mime_types[$ext]);
+    }
+    
+    readfile($path);
+    exit;
+}
+
 // Load composer
 require __DIR__ . '/../vendor/autoload.php';
 
